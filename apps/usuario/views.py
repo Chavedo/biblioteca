f0b18daffd1642905c90e74c535b49a93aa562d3
Mariano <amarianochavez@gmail.com>
2021-03-26 17:44:18 -0300

    + verification password in forms, + def post RegistrarUsuario

--- apps/usuario/views.py
+++ apps/usuario/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import login, logout
 from django.http.response import HttpResponseRedirect
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_protect
-from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 from django.views.generic.edit import FormView
 
 from apps.usuario.models import Usuario
@@ -50,3 +51,20 @@ class RegistrarUsuario(CreateView):
     form_class = FormularioUsuario
     template_name = 'usuario/crear_usuario.html'
     success_url = reverse_lazy('usuario:listar_usuario')
+
+    def post(self,request,*args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            nuevo_usuario = Usuario(
+               email = form.cleaned_data.get('email'),
+               username = form.cleaned_data.get('username'),
+               name = form.cleaned_data.get('name'),
+               last_name = form.cleaned_data.get('last_name'),
+            )
+            nuevo_usuario.set_password(form.cleaned_data.get('password1'))
+            nuevo_usuario.save()
+            return redirect('usuario:listar_usuario')
+        else:
+            return render(request,self.template_name,{'form':form})
+
+            
