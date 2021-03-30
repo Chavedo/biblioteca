39423a5c190daab0de963145ee6b72377d820485
Mariano <amarianochavez@gmail.com>
2021-03-30 16:09:07 -0300

    secret_key

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+from .secrets import secret_key
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g1cks4e1zkv%4h&pq!l8-6faq=20$#dq)-srn&3c=3oc&ut&mo'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
